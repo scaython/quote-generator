2360fd20cb8e0ba4cb55b2038e9a082719cdee41
scaython <ma.enes@hotmail.com>
2023-01-14 20:08:59 +0300

    First release

---
+++ app.js
@@ -0,0 +1,36 @@
+const quote = document.querySelector(".quote")
+const author = document.querySelector(".author>span")
+const next_btn = document.querySelector(".new-quote")
+const twit_btn = document.querySelector(".twitter-btn")
+
+// fetching quotes from api
+let quotes =[]
+
+function displayQuotes(e){
+    let newquote=quotes[Math.floor(Math.random()*quotes.length)]
+    quote.textContent=newquote.text
+    author.textContent=newquote.author
+}
+
+async function fetchQuotes(){
+    const api_url = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
+    try {
+        let response = await fetch(api_url);
+        quotes= await response.json()
+        
+        
+    } catch (error) {
+           quote.textContent="Try again something went wrong"
+    }
+}
+// twitter intent integration
+function tweetQuote(){
+       const twitter_url = `https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`;
+       window.open(twitter_url,"_blank");
+}
+//add event listeners
+twit_btn.addEventListener("click",tweetQuote)
+next_btn.addEventListener("click",displayQuotes)
+
+// call function on load
+fetchQuotes()
